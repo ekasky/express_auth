24d8548fde0cd9fbb0f8fbd64e6e94ee27e1c076
Evan Kasky <ekasky25@gmail.com>
2024-03-13 11:09:10 -0400

    Adds config varibales for session managment

--- config/config.js
+++ config/config.js
@@ -3,5 +3,11 @@ require("dotenv").config();
 module.exports = {
 
     PORT : process.env.PORT,
+    DB_HOST : process.env.DB_HOST, 
+    DB_USER : process.env.DB_USER, 
+    DB_PASS : process.env.DB_PASS, 
+    DB_NAME : process.env.DB_NAME, 
+    TIME : 60000 * 60, 
+    SESSION_SECRET : process.env.SESSION_SECRET,
 
 };
