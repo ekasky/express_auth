2168dec9e3ada2752502f1c4401a57c06eef454f
Evan Kasky <ekasky25@gmail.com>
2024-03-14 17:35:15 -0400

    Adds local registration 	- Adds endpoint which accepts users to register locally on the 	  site 	- Ensures username and email are unique for new users

---
+++ controllers/authControllers.js
@@ -0,0 +1,122 @@
+const validator = require("validator");
+const bcrypt = require("bcrypt");
+const user = require("../models/User");
+
+async function localRegisterController(req, res) {
+
+    const { email, username, password, first_name, last_name, gender } = req.body;
+
+    const response = {
+
+        "success": false,
+        "errors": {}
+
+    };
+
+    // Ensure all required feilds are supplied by the user
+   
+    if(!email) {
+
+        response.errors.email = "Email is required";
+
+    }
+
+    if(!username) {
+
+        response.errors.username = "Username is required";
+
+    }
+
+    if(!password) {
+
+        response.errors.password = "Password is required";
+
+    }
+
+    if(!first_name) {
+
+        response.errors.first_name = "First name is required";
+
+    }
+
+    if(!last_name) {
+
+        response.errors.last_name = "Last name is required";
+
+    }
+
+    if(!gender) {
+        
+        response.errors.gender = "Gender is required";
+
+    }
+
+    if(!["male", "female", "other"].includes(gender)) {
+
+        response.errors.gender = "Invalid Gender";
+
+    }
+
+    // Ensure email address is a valid email format
+    if(!validator.isEmail(email)) {
+
+        response.errors.email = "Invalid email address";
+
+        return res.status(400).json(response);
+
+    }
+
+    // Ensure passwors is strong
+    if(!validator.isStrongPassword(password)) {
+
+        response.errors.password = "Password to weak";
+
+        return res.status(400).json(response);
+
+    }
+
+    // Ensure the username or email is not already taken
+
+    const findEmail = await user.findOne({ where: {email} });
+    const findUser = await user.findOne({ where: {username} });
+
+    if(findEmail) {
+
+        response.errors.email = "Email already taken";
+
+    }
+
+    if(findUser) {
+
+        response.errors.username = "Username already taken";
+
+    }
+
+    if(Object.keys(response.errors).length !== 0) {
+
+        return res.status(400).json(response);
+
+    }
+
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    // Create the user in the db
+    const newUser = user.create({
+        email,
+        username,
+        password: hash,
+        first_name,
+        last_name,
+        gender
+    });
+    
+    response.success = true;
+
+    return res.status(201).json(response);
+
+}
+
+module.exports = {localRegisterController};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const { localRegisterController } = require("../controllers/authControllers");
+
 router.get("/test", (req, res) => res.status(200).json({message: "This is a test route"}));
+
+router.post("/local/register", localRegisterController);
 
 module.exports = router;
